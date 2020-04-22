bd1132fa8456e76bf660fc5635bc62b865419e80
Simran Sunil <simrans.cs18@sahyadri.edu.in>
2020-04-22 16:10:47 +0530

    First Commit

---
+++ lib/mycolor.dart
@@ -0,0 +1,11 @@
+class MyColor {
+  static int gridBackground=0xFF262626;//black[400]
+  static int gridColorTwoFour=0xFFFCE4EC;//pink[50]
+  static int fontColorTwoFour=0xFF262626;//black[400]
+  static int emptyGridBackground=0xFFCE93D8;//purple[200]
+  static int gridColorEightSixtyFourTwoFiftySix=0xFFE3F2FD;//blue[50]
+  static int gridColorOneTwentyEightFiveOneTwo=0xFFB2DFDB;//teal[100]
+  static int gridColorSixteenThirtyTwoOneZeroTwoFour=0xFFC5CAE9;//indigo[100]
+  static int gridColorWin=0xFF60d992;
+  static int transparentWhite=0x80FFFFFF;
+}
